63f21a3f81aef0fd02c531940ae7b75d2122e1b7
daniel12plo <43232637+daniel12plo@users.noreply.github.com>
2018-11-22 09:22:57 +0100

    Add files via upload

---
+++ bucle_12.py
@@ -0,0 +1,11 @@
+def bucle_12():
+    print "DAme 4 numeros al azar"
+    num1=0
+    num2=0
+    num3=0
+    num4=0
+    for i in range(1,5):
+        numero=input("numero ",i,": ")
+        print "Me acabas de dar el numero",numero
+
+bucle_12()
